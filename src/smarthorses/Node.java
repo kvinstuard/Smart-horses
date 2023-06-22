630e902cecb6118f49a4f669151d6b34367b4ad7
Kevin Marin <kevin.concha@correounivalle.edu.co>
2023-06-22 18:06:16 -0500

    Add: clase nodo

---
+++ src/smarthorses/Node.java
@@ -0,0 +1,76 @@
+package smarthorses;
+
+public class Node {
+
+    private String tipoNodo; //max, min or undefined
+    private int profundidad;
+    private int utilidad; //inicial MIN=+infinito, MAX=-infinito
+    private Node padre;
+    private int maxValue;
+    private int minValue;
+
+
+    public Node(){
+        this.tipoNodo = "INDEFINIDO";
+        this.profundidad = 0;
+        this.utilidad = 0;
+        this.maxValue = 0;
+        this.minValue = 0;
+    }
+
+
+
+
+    //GETTERS
+    public String getTipoNodo(){
+        return this.tipoNodo;
+    }
+
+    public int getProfundidad(){
+        return this.profundidad;
+    }
+
+    public int getUtilidad(){
+        return this.utilidad;
+    }
+
+    public int getMaxValue(){
+        return this.maxValue;
+    }
+
+    public int getMinValue(){
+        return this.minValue;
+    }
+
+    public Node getPadre(){
+        return this.padre;
+    }
+
+
+    //SETTERS
+    public void setTipoNodo(String tipo){
+        this.tipoNodo = tipo;
+    }
+
+    public void setProfundidad(int profundidad){
+        this.profundidad = profundidad;
+    }
+
+    public void setUtilidad(int utilidad){
+        this.utilidad = utilidad;
+    }
+
+    public void setPadre(Node nodo){
+        this.padre = nodo;
+    }
+
+    public void setMaxValue(int max){
+        this.maxValue = max;
+    }
+    
+    public void setMinValue(int min){
+        this.minValue = min;
+    }
+
+    
+}
